1bbeab62ff3a60f249e56d055cba39f429bf3b6c
Saidakbar Gaziev <si.gaziev@gmail.com>
2022-04-05 17:05:54 +0300

    basic commands are written

---
+++ commands.cpp
@@ -0,0 +1,5 @@
+//
+// Created by User on 05.04.2022.
+//
+
+#include "commands.h"
---
+++ commands.h
@@ -0,0 +1,16 @@
+//
+// Created by User on 05.04.2022.
+//
+
+#ifndef MATLANG_COMMANDS_H
+#define MATLANG_COMMANDS_H
+
+
+
+class commands {
+
+};
+
+
+
+#endif //MATLANG_COMMANDS_H
